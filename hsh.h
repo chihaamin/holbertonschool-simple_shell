a89c8928bc2dd711d2a2945fa52238afae38119d
chihaamin <amin.chiha@gmail.com>
2025-08-20 22:52:08 +0100

    fix header

--- hsh.h
+++ hsh.h
@@ -14,7 +14,8 @@ char *find_command_path(char *command, char **envp);
 int execute_command(char **args, char **envp, char *shell_name, int count);
 int handle_builtins(char **args, char **envp);
 void print_error(char *command, char *shell_name, int count);
-int process_input(char *line, char **envp, char *shell_name, int count);
+int process_input(char *line, char **envp, char *shell_name, int count,
+				  int *last_status);
 ssize_t read_input(char **line, size_t *len);
 
 #endif
