ef462cf03a27b25bbfc1589d83fce400883cefe3
chihaamin <amin.chiha@gmail.com>
2025-08-20 21:30:37 +0100

    Initialize hsh.h and main.c with basic structure and entry point

---
+++ hsh.h
@@ -0,0 +1,8 @@
+#ifndef HSH_H
+#define HSH_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#endif
---
+++ main.c
@@ -0,0 +1,9 @@
+#include "hsh.h"
+/**
+ * main - Entry point of the program
+ * Return: Always returns 0
+ */
+int main(void)
+{
+	return (0);
+}
